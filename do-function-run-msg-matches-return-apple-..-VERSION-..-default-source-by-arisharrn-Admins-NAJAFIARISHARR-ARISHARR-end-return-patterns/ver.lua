538fc91b4413f17f50ab164dd286f8ebdc1971c3
ARISHARR-ORG <a.abdollahpour138052@gmail.com>
2016-02-06 19:09:27 +0330

    Create ver.lua

---
+++ do-function-run-msg-matches-return-apple-..-VERSION-..-default-source-by-arisharrn-Admins-NAJAFIARISHARR-ARISHARR-end-return-patterns/ver.lua
@@ -0,0 +1,16 @@
+do
+
+function run(msg, matches)
+  return 'apple '.. VERSION .. [[ 
+  default source by arisharrn
+  Leaders:
+  @NAJAFIARISHARR
+  @ARISHARR
+end
+return {
+    patterns = {
+    "^[!/]([Vv]er)$"
+  }, 
+  run = run 
+}
+end
